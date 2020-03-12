6d6e59f31cd2737dd4896c7b2f6ee38b7b9c0af0
Massimiliano Iazzetta <maxiazzetta82@gmail.com>
2020-03-12 19:42:11 +0100

    esercizio tps

--- libri.sql
+++ libri.sql
@@ -1,16 +1,15 @@
-drop table if exists autori;
+
 create table autori (
     id integer primary key,
     cognome text not null,
     nome text
 );
 
-drop table if exists libri;
 create table libri (
     id integer primary key,
     titolo text not null,
     fk_autore integer not null
 );
 
-insert into autori (id,cognome, nome) values (1,"pascoli", "giovanni");
-insert into libri (id,titolo, fk_autore) values (1,"ciaociao", 1);
+insert into autori (id,cognome, nome) values (1,"King", "Stephen");
+insert into libri (id,titolo, fk_autore) values (1,"IT", 1);
