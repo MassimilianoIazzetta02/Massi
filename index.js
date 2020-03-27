198d2c8817d1514dd54b296589ee6ab733810164
Massimiliano Iazzetta <maxiazzetta82@gmail.com>
2020-03-27 23:41:33 +0100

    relazione

--- index.js
+++ index.js
@@ -11,8 +11,12 @@
        app.listen(3000);  
 }) ;
 
-   app.get("/", function(req, res) {
-       res.sendFile(path.join(__dirname,"public", "index.html"));
+app.get('/',function(req,res){
+    let sql = "SELECT libri.id, libri.titolo, group_concat(autori.cognome) as autori FROM libri INNER JOIN autori_libri ON libri.id=autori_libri.id_libro INNER JOIN autori ON autori_libri.id_autore=autori.id GROUP BY libri.id ORDER BY libri.titolo"
+    db.all(sql,(err,rows)=>{
+        if (err) res.send('ERRORE');
+        else res.render('index',{rows});
+    });
 });
 
    app.get ("/autori", function(req,res){
@@ -46,7 +50,12 @@ app.get ("/admin/libri", function(req,res){
                     res.render("admin_libri", {libri:rows});
     });
 });
-
+app.get ("/admin/relazioni", function(req,res){
+    const sql="select * from autori_libri;"
+    db.all(sql,function(err,rows) {
+        res.render("admin_relazioni", {relazioni:rows});
+    });
+});
 app.post("/modificautori", function(req, res) {
                const id = parseInt(req.body.id);
     sql="UPDATE Autori SET nome='"+req.body.nome+"',cognome='"+req.body.cognome+"' WHERE Autori.id ="+id;
@@ -88,7 +97,26 @@ app.post("/dellibro", function(req, res) {
                       db.run(sql);
                           res.redirect('/admin/libri');
 });
+app.post('/modrelazione',(req,res)=>{
+    const id=parseInt(req.body.id);
+    sql="UPDATE autori_libri SET id_autore='"+req.body.id_autore+"',id_libro='"+req.body.id_libro+"' WHERE autori_libri.id = "+id;
+    console.log(sql)
+    db.run(sql);
+    res.redirect('/admin/relazioni');
+});
 
+app.post('/delrelazione',(req,res)=>{
+    const id=parseInt(req.body.id);
+    let sql = `DELETE FROM autori_libri WHERE autori_libri.id=${id}`;
+    db.run(sql);
+    res.redirect('/admin/relazioni');
+});
+
+app.post("/addrelazione", function(req, res) {
+    sql="insert into autori_libri (id_autore, id_libro) values('"+req.body.id_autore+"','"+req.body.id_libro+"')";
+    db.run(sql);
+    res.redirect('/admin/relazioni');
+});
 app.use( function(req, res){
      res.status(404);
     res.sendFile(path.join (__dirname,"public","errore.html"));
--- libri.sql
+++ libri.sql
@@ -1,15 +1,62 @@
+.mode column
+.header on
 
+drop table if exists autori;
 create table autori (
     id integer primary key,
-    cognome text not null,
-    nome text
+    nome text,
+    cognome text not null
 );
 
+insert into autori (nome, cognome) values ('Giovanni', 'Boccaccio');
+insert into autori (cognome) values ('Pascoli');
+insert into autori (cognome) values ('Verga');
+insert into autori (cognome) values ('Autore2');
+insert into autori (cognome) values ('Autore3');
+
+select * from autori;
+select '';
+
+
+drop table if exists libri;
 create table libri (
     id integer primary key,
-    titolo text not null,
-    fk_autore integer not null
+    titolo text not null
 );
 
-insert into autori (id,cognome, nome) values (1,"King", "Stephen");
-insert into libri (id,titolo, fk_autore) values (1,"IT", 1);
+insert into libri (titolo) values ('Decamerone');
+insert into libri (titolo) values ('X agosto');
+insert into libri (titolo) values ('Mastro-Don Gesualdo');
+insert into libri (titolo) values ('Test senza autori');
+
+select * from libri;
+select '';
+
+drop table if exists autori_libri;
+create table autori_libri (
+    id integer primary key not null,
+    id_autore integer not null,
+    id_libro integer not null
+);
+
+insert into autori_libri (id_autore, id_libro) values (1, 1);
+insert into autori_libri (id_autore, id_libro) values (2, 2);
+insert into autori_libri (id_autore, id_libro) values (5, 3);
+insert into autori_libri (id_autore, id_libro) values (3, 3);
+insert into autori_libri (id_autore, id_libro) values (4, 3);
+
+
+select libri.titolo, group_concat(autori.cognome) as autori from libri
+    left join autori_libri on libri.id=autori_libri.id_libro
+    left join autori on autori.id=autori_libri.id_autore
+where libri.id = 3
+group by libri.id;
+select '';
+
+
+SELECT libri.id, libri.titolo, group_concat(autori.cognome) as autori FROM libri
+    LEFT JOIN autori_libri ON libri.id=autori_libri.id_libro
+    LEFT JOIN autori ON autori_libri.id_autore=autori.id
+GROUP BY libri.id
+ORDER BY libri.titolo;
+select '';
